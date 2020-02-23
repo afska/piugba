a05ec5759c318a9b969929bde4cc25b1eeb44cc1
Rodrigo Alfonso <rodri042@gmail.com>
2020-02-23 02:42:09 -0300

    Adding some rhythm code

--- src/scenes/SongScene.cpp
+++ src/scenes/SongScene.cpp
@@ -12,6 +12,11 @@ std::vector<Sprite*> SongScene::sprites() {
   return {animation.get()};
 }
 
+int last_beat = 0;
+int count = -3;
+int velocity = 10;
+bool to_left = false;
+
 void SongScene::load() {
   foregroundPalette = std::unique_ptr<ForegroundPaletteManager>(
       new ForegroundPaletteManager(sharedPal, sizeof(sharedPal)));
@@ -22,8 +27,8 @@ void SongScene::load() {
 
   animation = builder.withData(lamaTiles, sizeof(lamaTiles))
                   .withSize(SIZE_32_32)
-                  .withAnimated(6, 3)
-                  .withLocation(50, 50)
+                  .withAnimated(6, 2)
+                  .withLocation(70, 50)
                   .buildPtr();
 
   TextStream::instance().setText("AHI TA VITEH!", 3, 8);
@@ -33,15 +38,34 @@ void SongScene::load() {
 
 void SongScene::tick(u16 keys) {
   engine->getTimer()->onvblank();  // TODO: MOVE
-  TextStream::instance().setText(engine->getTimer()->to_string(), 18, 1);
 
-  if (keys & KEY_LEFT) {
-    animation->flipHorizontally(true);
-  } else if (keys & KEY_RIGHT) {
-    animation->flipHorizontally(false);
-  } else if (keys & KEY_UP) {
-    animation->flipVertically(true);
-  } else if (keys & KEY_DOWN) {
-    animation->flipVertically(false);
+  // 60000-----157beats
+  // totalmsecs-----x = totalmsecs*157/60000
+  int beat = (engine->getTimer()->getTotalMsecs() * 157) / 60000;  // 157 bpm
+  if (beat != last_beat) {
+    int delta = sgn(velocity);
+    count += delta;
+    animation->moveTo(animation->getX() + velocity, animation->getY());
+    if (abs(count) >= 4) {
+      velocity = -velocity;
+      to_left = !to_left;
+      animation->flipHorizontally(to_left);
+    }
   }
+  TextStream::instance().setText(std::to_string(count), 10, 1);
+  last_beat = beat;
+
+  int is_odd = beat & 1;
+  TextStream::instance().setText("             ", !is_odd ? 18 : 15, 1);
+  TextStream::instance().setText(engine->getTimer()->to_string(), is_odd ? 18 : 15, 1);
+
+  // if (keys & KEY_LEFT) {
+  //   animation->flipHorizontally(true);
+  // } else if (keys & KEY_RIGHT) {
+  //   animation->flipHorizontally(false);
+  // } else if (keys & KEY_UP) {
+  //   animation->flipVertically(true);
+  // } else if (keys & KEY_DOWN) {
+  //   animation->flipVertically(false);
+  // }
 }
