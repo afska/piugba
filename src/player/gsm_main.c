6c0ef20086f02fa8e759c95a1d2bf90c540a12a4
Rodrigo Alfonso <rodri042@gmail.com>
2020-02-23 02:07:22 -0300

    Removing keyboard controls

--- src/player/gsm_main.c
+++ src/player/gsm_main.c
@@ -8,9 +8,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>  // for memset
+#include "../utils/gbfs.h"
 #include "core/gsm.h"
 #include "core/private.h" /* for sizeof(struct gsm_state) */
-#include "../utils/gbfs.h"
 
 #define CMD_START_SONG 0x0400
 #define TIMER_16MHZ 0
@@ -47,7 +47,6 @@ void streaming_run(void (*update)()) {
   unsigned short last_joy = 0x3ff;
   unsigned int cur_song = (unsigned int)(-1);
   int last_sample = 0;
-  unsigned int locked = 0;
 
   unsigned int nframes = 0;
 
@@ -55,29 +54,15 @@ void streaming_run(void (*update)()) {
     update();
 
     unsigned short j = (REG_KEYINPUT & 0x3ff) ^ 0x3ff;
-    unsigned short cmd = j & (~last_joy | KEY_R | KEY_L);
+    unsigned short cmd = j & (~last_joy);
     signed char* dst_pos = double_buffers[cur_buffer];
-
     last_joy = j;
 
     // At end of track, proceed to the next
     if (src_pos >= src_end) {
-      cmd |= KEY_RIGHT;
-    }
-
-    if (cmd & KEY_RIGHT) {
       cur_song++;
       if (cur_song >= gbfs_count_objs(fs)) {
         cur_song = 0;
-      }
-      cmd |= CMD_START_SONG;
-    }
-
-    if (cmd & KEY_LEFT) {
-      if (cur_song == 0) {
-        cur_song = gbfs_count_objs(fs) - 1;
-      } else {
-        cur_song--;
       }
       cmd |= CMD_START_SONG;
     }
@@ -86,56 +71,40 @@ void streaming_run(void (*update)()) {
       char name[25];
       gsm_init(&decoder);
       src = gbfs_get_nth_obj(fs, cur_song, name, &src_len);
-
-      // If reached by seek, go near end of the track.
-      // Otherwise, go to the start.
-      if (cmd & KEY_L) {
-        src_pos = src + src_len - 33 * 60;
-      } else {
-        src_pos = src;
-      }
+      src_pos = src;
       src_end = src + src_len;
     }
 
-    if (locked & KEY_START) { /* if paused */
-      for (j = 304 / 2; j > 0; j--) {
-        *dst_pos++ = last_sample >> 8;
-        *dst_pos++ = last_sample >> 8;
-        *dst_pos++ = last_sample >> 8;
-        *dst_pos++ = last_sample >> 8;
-      }
-    } else {
-      for (j = 304 / 4; j > 0; j--) {
-        int cur_sample;
-        if (decode_pos >= 160) {
-          if (src_pos < src_end) {
-            gsm_decode(&decoder, src_pos, out_samples);
-          }
-          src_pos += sizeof(gsm_frame);
-          decode_pos = 0;
+    for (j = 304 / 4; j > 0; j--) {
+      int cur_sample;
+      if (decode_pos >= 160) {
+        if (src_pos < src_end) {
+          gsm_decode(&decoder, src_pos, out_samples);
         }
-
-        /* 2:1 linear interpolation */
-        cur_sample = out_samples[decode_pos++];
-        *dst_pos++ = (last_sample + cur_sample) >> 9;
-        *dst_pos++ = cur_sample >> 8;
-        last_sample = cur_sample;
-
-        cur_sample = out_samples[decode_pos++];
-        *dst_pos++ = (last_sample + cur_sample) >> 9;
-        *dst_pos++ = cur_sample >> 8;
-        last_sample = cur_sample;
-
-        cur_sample = out_samples[decode_pos++];
-        *dst_pos++ = (last_sample + cur_sample) >> 9;
-        *dst_pos++ = cur_sample >> 8;
-        last_sample = cur_sample;
-
-        cur_sample = out_samples[decode_pos++];
-        *dst_pos++ = (last_sample + cur_sample) >> 9;
-        *dst_pos++ = cur_sample >> 8;
-        last_sample = cur_sample;
+        src_pos += sizeof(gsm_frame);
+        decode_pos = 0;
       }
+
+      /* 2:1 linear interpolation */
+      cur_sample = out_samples[decode_pos++];
+      *dst_pos++ = (last_sample + cur_sample) >> 9;
+      *dst_pos++ = cur_sample >> 8;
+      last_sample = cur_sample;
+
+      cur_sample = out_samples[decode_pos++];
+      *dst_pos++ = (last_sample + cur_sample) >> 9;
+      *dst_pos++ = cur_sample >> 8;
+      last_sample = cur_sample;
+
+      cur_sample = out_samples[decode_pos++];
+      *dst_pos++ = (last_sample + cur_sample) >> 9;
+      *dst_pos++ = cur_sample >> 8;
+      last_sample = cur_sample;
+
+      cur_sample = out_samples[decode_pos++];
+      *dst_pos++ = (last_sample + cur_sample) >> 9;
+      *dst_pos++ = cur_sample >> 8;
+      last_sample = cur_sample;
     }
 
     VBlankIntrWait();
