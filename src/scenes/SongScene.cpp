ce5d0a8ab8f0b66bcb04cc065ee79ae247b74869
Rodrigo Alfonso <rodri042@gmail.com>
2020-02-23 01:29:04 -0300

    Adding timer

--- src/scenes/SongScene.cpp
+++ src/scenes/SongScene.cpp
@@ -1,7 +1,7 @@
+#include "SongScene.h"
+#include <libgba-sprite-engine/background/text_stream.h>
 #include <libgba-sprite-engine/palette/palette_manager.h>
 #include <libgba-sprite-engine/sprites/sprite_builder.h>
-#include <libgba-sprite-engine/background/text_stream.h>
-#include "SongScene.h"
 #include "data/lama.h"
 
 std::vector<Background*> SongScene::backgrounds() {
@@ -27,9 +27,14 @@ void SongScene::load() {
                   .buildPtr();
 
   TextStream::instance().setText("AHI TA VITEH!", 3, 8);
+
+  engine->getTimer()->start();
 }
 
 void SongScene::tick(u16 keys) {
+  engine->getTimer()->onvblank(); // TODO: MOVE
+  TextStream::instance().setText(engine->getTimer()->to_string(), 18, 1);
+
   if (keys & KEY_LEFT) {
     animation->flipHorizontally(true);
   } else if (keys & KEY_RIGHT) {
