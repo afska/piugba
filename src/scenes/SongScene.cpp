a4754861927bc797d141e2cdca5adb073009afd0
Rodrigo Alfonso <rodri042@gmail.com>
2020-03-11 22:29:32 -0300

    Removing weird ;

--- src/scenes/SongScene.cpp
+++ src/scenes/SongScene.cpp
@@ -3,7 +3,7 @@
 #include <libgba-sprite-engine/sprites/sprite_builder.h>
 #include "data/content/BeethovenVirus.h"
 #include "data/content/compiled/shared_palette.h"
-#include "utils/SpriteUtils.h";
+#include "utils/SpriteUtils.h"
 
 const u32 BPM = 156;
 const u32 INITIAL_OFFSET = 150;
