c019125e84d1d4bfe782c7a7283814be8e59ed89
Rodrigo Alfonso <rodri042@gmail.com>
2020-03-26 04:02:35 -0300

    Adding a Song model

---
+++ src/gameplay/Song.cpp
@@ -0,0 +1,9 @@
+#include "Song.h"
+
+extern "C" {
+#include "utils/gbfs.h"
+}
+
+Song* song_parse(char* fileName) {}
+
+void song_free(Song* song) {}
---
+++ src/gameplay/SongReader.h
@@ -0,0 +1,11 @@
+#ifndef SONG_READER_H
+#define SONG_READER_H
+
+#include "Song.h"
+
+class SongReader {
+ public:
+  SongReader(Song* song);
+};
+
+#endif  // SONG_READER_H
