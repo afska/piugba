5467f96ebfea15ef2ea0aae692035f0d5c9d101c
Rodrigo Alfonso <rodri042@gmail.com>
2020-03-26 21:55:46 -0300

    Adding constants with event masks

--- src/gameplay/models/Event.h
+++ src/gameplay/models/Event.h
@@ -3,18 +3,20 @@
 
 #include <libgba-sprite-engine/gba/tonc_core.h>
 
+enum EventType { NOTE, HOLD_START, HOLD_TAIL, STOP, SET_TEMPO, SET_SPEED };
+
+#define EVENT_ARROW_FLAGS 5
+#define EVENT_ARROW_DOWNLEFT /*   */ 0b00001000
+#define EVENT_ARROW_UPLEFT /*     */ 0b00010000
+#define EVENT_ARROW_CENTER /*     */ 0b00100000
+#define EVENT_ARROW_UPRIGHT /*    */ 0b01000000
+#define EVENT_ARROW_DOWNRIGHT /*  */ 0b10000000
+
 typedef struct {
   u32 timestamp;  // in ms
   u8 data;
   /* {
-        [bits 0-2] type (0 = NOTE,
-                         1 = HOLD_START,
-                         2 = HOLD_TAIL,
-                         3 = STOP,
-                         4 = SET_TEMPO,
-                         5 = SET_SPEED,
-                         ...rest = UNUSED
-                         )
+        [bits 0-2] type (see EnumType)
         [bits 3-7] data (5-bit array with the arrows)
       }
   */
