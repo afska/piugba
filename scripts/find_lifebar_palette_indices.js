8ccfe9bd16009a19e88000e8e37b7fb106005b05
Rodrigo Alfonso <rodri042@gmail.com>
2020-03-30 06:10:45 -0300

    Adding palette colors and indices

---
+++ scripts/find_lifebar_palette_indices.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const FILE_PATH = "../src/data/content/compiled/shared_palette.c";
+const LIFEBAR_COLORS = [
+  0x007F, 0x10F9,
+  0x019A, 0x1DB6,
+  0x0A7E, 0x063B,
+  0x02FE, 0x02BC,
+  0x039F, 0x037E,
+  0x03DC, 0x039B,
+  0x03F9, 0x03B7,
+  0x03CE, 0x036F,
+  0x23EF, 0x03A8
+];
+
+const file = fs.readFileSync(FILE_PATH).toString();
+const content = file.substring(file.indexOf("0x"), file.indexOf("};"));
+const palette = eval(`[${content}]`);
+
+const indices = LIFEBAR_COLORS.map((color) => palette.indexOf(color));
+
+console.log(JSON.stringify({ colors: LIFEBAR_COLORS, indices }));
--- src/objects/LifeBar.cpp
+++ src/objects/LifeBar.cpp
@@ -5,6 +5,10 @@
 
 const u32 POSITION_X = 15;
 const int POSITION_Y = -11 + 2;
+const u16 PALETTE_COLORS[] = {127, 4345, 410, 7606, 2686, 1595, 766, 700,  927,
+                              894, 988,  923, 1017, 951,  974,  879, 9199, 936};
+const u8 PALETTE_INDICES[] = {173, 175, 179, 180, 188, 186, 194, 190, 202,
+                              197, 203, 199, 204, 198, 196, 193, 201, 192};
 
 LifeBar::LifeBar() {
   SpriteBuilder<Sprite> builder;
