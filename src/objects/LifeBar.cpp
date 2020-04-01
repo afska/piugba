4c92f48660eefa336bd7a98871dfdf5e6e44c2a3
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-01 06:38:09 -0300

    Finishing LifeBar animations

--- src/objects/LifeBar.cpp
+++ src/objects/LifeBar.cpp
@@ -1,4 +1,5 @@
 #include "LifeBar.h"
+#include <libgba-sprite-engine/gba/tonc_math.h>
 #include <libgba-sprite-engine/gba_engine.h>
 #include <libgba-sprite-engine/sprites/sprite_builder.h>
 #include "data/content/compiled/spr_lifebar.h"
@@ -18,7 +19,8 @@ const COLOR DISABLED_COLOR = 0x0000;
 const COLOR DISABLED_COLOR_BORDER = 0x2529;
 const COLOR CURSOR_COLOR = 0x7FD8;
 const COLOR CURSOR_COLOR_BORDER = 0x7734;
-const COLOR BLINK_COLOR = 0x7FFF;
+const COLOR BLINK_MIN_COLOR = 127;
+const COLOR BLINK_MAX_COLOR = 0x7FFF;
 
 LifeBar::LifeBar() {
   SpriteBuilder<Sprite> builder;
@@ -38,7 +40,7 @@ void LifeBar::tick(ForegroundPaletteManager* foregroundPalette) {
 
   animatedFlag = !animatedFlag;
 
-  if (animatedValue > value - ANIMATION_OFFSET && wait == 0) {
+  if (animatedValue > max(MIN_VALUE, value - ANIMATION_OFFSET) && wait == 0) {
     animatedValue--;
     wait = WAIT_TIME;
   } else if (wait > 0)
@@ -50,14 +52,17 @@ void LifeBar::paint(ForegroundPaletteManager* foregroundPalette) {
   for (u32 i = 0; i < sizeof(PALETTE_INDICES); i++) {
     COLOR color = PALETTE_COLORS[i];
 
-    if (value < MAX_VALUE) {
+    if (value == MIN_VALUE)
+      color = (odd ? DISABLED_COLOR_BORDER
+                   : (animatedFlag ? BLINK_MIN_COLOR : DISABLED_COLOR));
+    else if (value < MAX_VALUE) {
       if (i >= animatedValue * 2 - 2)
         color = (odd ? DISABLED_COLOR_BORDER : DISABLED_COLOR);
 
       if (i >= value * 2 - 2 && i < value * 2)
         color = (odd ? CURSOR_COLOR_BORDER : CURSOR_COLOR);
-    } else if (animatedFlag && !odd && i >= BLINK_START * 2)
-      color = BLINK_COLOR;
+    } else if (animatedFlag && !odd && i >= BLINK_START)
+      color = BLINK_MAX_COLOR;
 
     foregroundPalette->change(0, PALETTE_INDICES[i], color);
     odd = !odd;
