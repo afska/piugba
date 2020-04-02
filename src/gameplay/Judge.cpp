c8b4de743ce09f12c049e3fcac1fc422cbbc7655
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-02 05:35:23 -0300

    Adding new keys and extracting constants

--- src/gameplay/Judge.cpp
+++ src/gameplay/Judge.cpp
@@ -1,5 +1,10 @@
 #include "Judge.h"
 
+const u32 MISS_OFFSET = 8;
+const u32 BAD_OFFSET = 6;
+const u32 GOOD_OFFSET = 4;
+const u32 GREAT_OFFSET = 2;
+
 Judge::Judge() {}
 
 void Judge::onPress(ArrowType type,
@@ -9,14 +14,15 @@ void Judge::onPress(ArrowType type,
     int diff = it->get()->getY() - ARROW_CORNER_MARGIN_Y;
     u32 absDiff = (u32)abs(diff);
 
-    if (!it->isEnding() && it->type == type && absDiff < ARROW_SPEED * 8) {
-      if (absDiff >= ARROW_SPEED * 6) {
+    if (!it->isEnding() && it->type == type &&
+        absDiff < ARROW_SPEED * MISS_OFFSET) {
+      if (absDiff >= ARROW_SPEED * BAD_OFFSET) {
         score->update(FeedbackType::BAD);
         it->markAsPressed();
-      } else if (absDiff >= ARROW_SPEED * 4) {
+      } else if (absDiff >= ARROW_SPEED * GOOD_OFFSET) {
         score->update(FeedbackType::GOOD);
         it->markAsPressed();
-      } else if (absDiff >= ARROW_SPEED * 2) {
+      } else if (absDiff >= ARROW_SPEED * GREAT_OFFSET) {
         score->update(FeedbackType::GREAT);
         it->press();
       } else {
