3bed1799d321995e3a918d51510b36e50205fb6f
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-12 06:25:42 -0300

    Avoiding Judge from firing MISSes from hold arrows

--- src/gameplay/Judge.cpp
+++ src/gameplay/Judge.cpp
@@ -33,7 +33,8 @@ void Judge::onPress(Arrow* arrow) {
 }
 
 void Judge::onOut(Arrow* arrow) {
-  if (!arrow->getIsPressed()) {
+  bool isUniqueArrow = arrow->type == ArrowType::UNIQUE;
+  if (!arrow->getIsPressed() && isUniqueArrow) {
     FeedbackType result = onResult(arrow, FeedbackType::MISS);
     if (result == FeedbackType::UNKNOWN)
       return;
