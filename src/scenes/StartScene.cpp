22ddd1fff00acdd9c2f2da037b44bd26d82b13bf
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-18 05:26:12 -0300

    Playing with alpha blend

--- src/scenes/StartScene.cpp
+++ src/scenes/StartScene.cpp
@@ -97,29 +97,38 @@ void StartScene::tick(u16 keys) {
   if (i == 0) {
     i = 1;
 
-    REG_BG0CNT = BG_CBB(3) | BG_SBB(30) | BG_8BPP | BG_REG_32x32;
+    REG_BG0CNT = BG_CBB(3) | BG_SBB(31) | BG_8BPP | BG_REG_32x32;
 
     // Set up palette memory, colors are 15bpp
-    pal_bg_mem[0] = 0x77DF;  // base color (cream)
-    pal_bg_mem[1] = 0x4588;  // blue
-    pal_bg_mem[2] = 127;     // red
+    pal_bg_mem[0] = 0;      // base color (black)
+    pal_bg_mem[255] = 127;  // red
 
     // Set up an 8x8 tile 1
     for (int line = 0; line < 8; line++) {
       // update charblock 0, tile 1, line i * 2
 
-      tile8_mem[3][1].data[line * 2] =
-          (1 << 0) + (2 << 8) + (1 << 16) + (2 << 24);
+      tile8_mem[3][255].data[line * 2] =
+          (255 << 0) + (255 << 8) + (255 << 16) + (255 << 24);
       //    blue       red        blue        red
-      tile8_mem[3][1].data[line * 2 + 1] =
-          (1 << 0) + (1 << 8) + (2 << 16) + (2 << 24);
+      tile8_mem[3][255].data[line * 2 + 1] =
+          (255 << 0) + (255 << 8) + (255 << 16) + (255 << 24);
       //    blue       blue        red        red
     }
 
     // Set up a map, draw 10 tiles starting from tile 6
-    for (int i = 6; i < 6 + 10; i++)
+    for (int i = 6; i < 6 + 150; i++)
       // update screenblock 24, screenblock entry i
-      se_mem[30][i] = 1;  // set tile 1
+      se_mem[31][i] = 255;  // set tile 1
+
+    REG_BLDCNT = 0b0000001001000001;    // blend BG0 on top of BG1
+    REG_BLDALPHA = 0b0000100000011000;  // BG0 weight: 11000, BG1 weight: 1000
+
+    /*
+magick extravaganza.bmp -background black -gravity north -splice 0x70
+extravaganza.bmp extravaganza.bmp -background black -gravity south -splice 0x10
+extravaganza.bmp -append -background black -gravity south -splice 0x30
+extrapend.png
+    */
   }
 
   if (keys & KEY_ANY && !engine->isTransitioning()) {
