d7a3244f9f9ab32cb4ef661f3fe96292122d1e55
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-19 07:25:23 -0300

    Finally understanding masking

--- src/scenes/StartScene.cpp
+++ src/scenes/StartScene.cpp
@@ -95,28 +95,38 @@ void StartScene::tick(u16 keys) {
   if (i == 0) {
     i = 1;
 
-    REG_BG0CNT = BG_CBB(3) | BG_SBB(31) | BG_8BPP | BG_REG_32x32;
+    REG_BG0CNT = BG_CBB(3) | BG_SBB(30) | BG_8BPP | BG_REG_32x32;
 
     // Set up palette memory, colors are 15bpp
     // pal_bg_mem[0] = 0;      // base color (black)
     pal_bg_mem[255] = 127;  // red
 
-    // Set up an 8x8 tile 255
+    // Set up an 8x8 tile 254
+    // TRANSPARENT TILE
     for (int line = 0; line < 8; line++) {
-      // update charblock 3, tile 1, line i * 2
+      // update charblock 3, tile 254, line i * 2
+
+      tile8_mem[3][254].data[line * 2] =
+          (0 << 0) + (0 << 8) + (0 << 16) + (0 << 24);
+      tile8_mem[3][254].data[line * 2 + 1] =
+          (0 << 0) + (0 << 8) + (0 << 16) + (0 << 24);
+    }
+
+    // Set up an 8x8 tile 255
+    // RED TILE
+    for (int line = 0; line < 8; line++) {
+      // update charblock 3, tile 255, line i * 2
 
       tile8_mem[3][255].data[line * 2] =
           (255 << 0) + (255 << 8) + (255 << 16) + (255 << 24);
-      //    blue       red        blue        red
       tile8_mem[3][255].data[line * 2 + 1] =
           (255 << 0) + (255 << 8) + (255 << 16) + (255 << 24);
-      //    blue       blue        red        red
     }
 
-    // Set up a map, draw 1000 tiles starting from tile 6
-    for (int i = 6; i < 6 + 1000; i++)
-      // update screenblock 31, screenblock entry i
-      se_mem[31][i] = 255;  // set tile 255
+    // Set up a map, draw tiles
+    for (int i = 0; i < 32 * 32; i++)
+      // update screenblock 30, screenblock entry i
+      se_mem[30][i] = i < 7 ? 254 : 255;  // set tile 255 or 254 (transparent)
 
     REG_BLDCNT = 0b0000001001000001;    // blend BG0 on top of BG1
     REG_BLDALPHA = 0b0000100000011000;  // BG0 weight: 11000, BG1 weight: 1000
