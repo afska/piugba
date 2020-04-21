41dcf7eaa6e2fb34643e43034dcffe6b3fa913cf
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-21 05:55:11 -0300

    Extracting bg_selectionmask

---
+++ src/data/bg_selectionmask.h
@@ -0,0 +1,8 @@
+#ifndef BG_SELECTIONMASK_H
+#define BG_SELECTIONMASK_H
+
+extern const unsigned int BG_SELECTIONMASK_TILES[];
+extern const unsigned int BG_SELECTIONMASK_MAP[];
+extern const unsigned int BG_SELECTIONMASK_PALETTE[];
+
+#endif  // BG_SELECTIONMASK_H
