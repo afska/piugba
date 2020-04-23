6b9821cc76ee4b55096eed6c10541aedff1f9947
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-23 10:55:23 -0300

    Adding all difficulty level sprites

--- src/objects/Difficulty.cpp
+++ src/objects/Difficulty.cpp
@@ -5,8 +5,15 @@
 #include "data/content/compiled/spr_difficulties.h"
 #include "utils/SpriteUtils.h"
 
-const u32 POSITION_X = 8;
-const u32 POSITION_Y = 100;
+const u32 POSITION_X = 79;
+const u32 POSITION_Y = 15;
+const u32 FRAME_NOR = 0;
+const u32 FRAME_MAL = 1;
+const u32 FRAME_HARD = 2;
+const u32 FRAME_CRAZY = 3;
+const u32 OFFSET_X_MAL = 40;
+const u32 OFFSET_X_HARD = 14;
+const u32 OFFSET_X_CRAZY = 10;
 
 Difficulty::Difficulty() {
   SpriteBuilder<Sprite> builder1;
@@ -22,9 +29,36 @@ Difficulty::Difficulty() {
                     .withLocation(POSITION_X, POSITION_Y)
                     .buildPtr();
   SPRITE_reuseTiles(rightSprite.get());
+  SPRITE_goToFrame(rightSprite.get(), FRAME_MAL);
+
+  setValue(value);
 }
 
-void Difficulty::setValue(DifficultyLevel value) {}
+void Difficulty::setValue(DifficultyLevel value) {
+  this->value = value;
+
+  switch (value) {
+    case DifficultyLevel::NORMAL:
+      SPRITE_goToFrame(leftSprite.get(), FRAME_NOR);
+      leftSprite->moveTo(POSITION_X, POSITION_Y);
+      rightSprite->moveTo(POSITION_X + OFFSET_X_MAL, POSITION_Y);
+      break;
+    case DifficultyLevel::HARD:
+      SPRITE_goToFrame(leftSprite.get(), FRAME_HARD);
+      leftSprite->moveTo(POSITION_X + OFFSET_X_HARD, POSITION_Y);
+      SPRITE_hide(rightSprite.get());
+      break;
+    case DifficultyLevel::CRAZY:
+      SPRITE_goToFrame(leftSprite.get(), FRAME_CRAZY);
+      leftSprite->moveTo(POSITION_X + OFFSET_X_CRAZY, POSITION_Y);
+      SPRITE_hide(rightSprite.get());
+      break;
+    default:
+      SPRITE_hide(leftSprite.get());
+      SPRITE_hide(rightSprite.get());
+      break;
+  }
+}
 
 DifficultyLevel Difficulty::getValue() {
   return value;
