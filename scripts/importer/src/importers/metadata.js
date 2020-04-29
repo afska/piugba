33a81ebb6b5af2e5a7fad3398b1688c3b9c2ff47
Rodrigo Alfonso <rodri042@gmail.com>
2020-04-29 19:08:03 -0300

    Adding simfile integrity checks

--- scripts/importer/src/importers/metadata.js
+++ scripts/importer/src/importers/metadata.js
@@ -7,26 +7,46 @@ const utils = require("../utils");
 const _ = require("lodash");
 
 const EXTENSION = "pius";
+const KNOWN_CHANNELS = ["ORIGINAL", "KPOP", "WORLD"];
+const NON_NUMERIC_LEVELS = ["NORMAL", "HARD", "CRAZY"];
 
 module.exports = (name, filePath, outputPath) => {
   const content = fs.readFileSync(filePath).toString();
   const { metadata, charts } = new Simfile(content, name);
 
-  if (_.isEmpty(charts)) throw new Error("no_charts");
+  checkIntegrity(metadata, charts);
   completeMissingInformation(metadata, charts);
+  const selectedCharts = charts.filter((it) =>
+    _.includes(NON_NUMERIC_LEVELS, it.header.difficulty)
+  );
 
-  const simfile = { metadata, charts };
+  const simfile = { metadata, charts: selectedCharts };
   const output = new SongSerializer(simfile).serialize();
 
   fs.writeFileSync($path.join(outputPath, `${name}.${EXTENSION}`), output);
 };
 
+const checkIntegrity = (metadata, charts) => {
+  if (_.isEmpty(metadata.id)) throw new Error("missing_id");
+  if (_.isEmpty(metadata.title)) throw new Error("missing_title");
+  if (_.isEmpty(metadata.artist)) throw new Error("missing_artist");
+  if (!_.isFinite(metadata.sampleStart)) throw new Error("invalid_samplestart");
+  if (!_.isFinite(metadata.sampleLength))
+    throw new Error("invalid_samplelength");
+  if (_.isEmpty(charts)) throw new Error("no_charts");
+};
+
 const completeMissingInformation = (metadata, charts) => {
   if (metadata.channel === "UNKNOWN") {
-    console.log(
-      "-> channels: ".bold + `(0 = ORIGINAL, 1 = KPOP, 2 = WORLD)`.cyan
+    const channelOptions = KNOWN_CHANNELS.map(
+      (name, i) => `${i} = ${name}`
+    ).join(", ");
+
+    console.log("-> channels: ".bold + `(${channelOptions})`.cyan);
+    const channel = utils.insistentChoice(
+      "What channel?",
+      _.range(KNOWN_CHANNELS.length)
     );
-    const channel = utils.insistentChoice("What channel?", _.range(3));
     metadata.channel = _.keys(Channels)[parseInt(channel)];
   }
 
@@ -39,6 +59,8 @@ const completeMissingInformation = (metadata, charts) => {
     ["y", "n"],
     "red"
   );
+
+  return;
 };
 
 const setDifficulty = (charts, difficultyName) => {
