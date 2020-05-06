97bcab042e1dfd5413651ef841336bdf3578606e
Rodrigo Alfonso <rodri042@gmail.com>
2020-05-06 06:57:53 -0300

    Fixing the speed formula

--- src/scenes/SongScene.cpp
+++ src/scenes/SongScene.cpp
@@ -177,7 +177,8 @@ void SongScene::updateArrows() {
     bool isPressing = arrowHolders[direction]->getIsPressed();
 
     int timeLeft = (int)chart->events[it->eventIndex].timestamp - chartMsecs;
-    int newY = Div(timeLeft * ARROW_INITIAL_Y, chartReader->timeNeeded);
+    int newY =
+        ARROW_FINAL_Y + Div(timeLeft * ARROW_DISTANCE, chartReader->timeNeeded);
     ArrowState arrowState = it->tick(chartReader->hasStopped, isPressing, newY);
     if (chartReader->hasStopped)  // TODO: Fix hasBeenPressedNow
       return;
