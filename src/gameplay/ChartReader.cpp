e7b3e75e5d443801d47f73985ec4dafc6d2a5c67
Rodrigo Alfonso <rodri042@gmail.com>
2020-06-13 05:05:05 -0300

    FIX: Not resetting maxArrowTimeJump after bpm changes

--- src/gameplay/ChartReader.cpp
+++ src/gameplay/ChartReader.cpp
@@ -160,8 +160,7 @@ void ChartReader::processNextEvents() {
               if (event->param3 > 0) {
                 u32 arrowTimeDiff = abs((int)targetArrowTime - (int)arrowTime);
                 maxArrowTimeJump = Div(arrowTimeDiff, event->param3);
-              } else
-                resetMaxArrowTimeJump();
+              }
             } else
               syncArrowTime();
             return true;
