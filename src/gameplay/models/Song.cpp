dcba87b99163ddb1e3951ec305c1fb66d933cb48
Rodrigo Alfonso <rodri042@gmail.com>
2020-08-31 03:36:28 -0300

    Parsing new data

--- src/gameplay/models/Song.cpp
+++ src/gameplay/models/Song.cpp
@@ -25,6 +25,10 @@ Song* SONG_parse(const GBFS_FILE* fs, SongFile* file, bool full) {
   song->sampleStart = parse_u32le(data, &cursor);
   song->sampleLength = parse_u32le(data, &cursor);
 
+  song->applyTo[DifficultyLevel::NORMAL] = parse_u8(data, &cursor);
+  song->applyTo[DifficultyLevel::HARD] = parse_u8(data, &cursor);
+  song->applyTo[DifficultyLevel::CRAZY] = parse_u8(data, &cursor);
+  song->isBoss = parse_u8(data, &cursor);
   song->pixelate = parse_u8(data, &cursor);
   song->jump = parse_u8(data, &cursor);
   song->reduce = parse_u8(data, &cursor);
@@ -86,7 +90,12 @@ Channel SONG_getChannel(const GBFS_FILE* fs, SongFile* file) {
   auto data = (u8*)gbfs_get_obj(fs, file->getMetadataFile().c_str(), &length);
 
   u32 cursor = TITLE_LEN + ARTIST_LEN;
-  return static_cast<Channel>(parse_u8(data, &cursor));
+  auto channel = static_cast<Channel>(parse_u8(data, &cursor));
+
+  cursor += 4 /* lastMillisecond */ + 4 /* sampleStart */ +
+            4 /* sampleLength */ + 3 /* applyTo */;
+  bool isBoss = parse_u8(data, &cursor);
+  return isBoss ? Channel::BOSS : channel;
 }
 
 Chart* SONG_findChartByDifficultyLevel(Song* song,
