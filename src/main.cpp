9a9edd6021b6c1779169923654181ebe34184753
Rodrigo Alfonso <rodri042@gmail.com>
2020-11-30 20:00:49 -0300

    Updating gba-link-connection to v2.1.4

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,7 @@ const char* SAVEFILE_TYPE_HINT = "SRAM_Vnnn\0\0";
 void setUpInterrupts();
 void synchronizeSongStart();
 static std::shared_ptr<GBAEngine> engine{new GBAEngine()};
-LinkConnection* linkConnection = new LinkConnection(LinkConnection::BAUD_RATE_1,
-                                                    LINK_DEFAULT_TIMEOUT,
-                                                    LINK_DEFAULT_REMOTE_TIMEOUT,
-                                                    SYNC_BUFFER_SIZE);
+LinkConnection* linkConnection = new LinkConnection();
 Syncer* syncer = new Syncer();
 static const GBFS_FILE* fs = find_first_gbfs_file(0);
 
@@ -87,7 +84,6 @@ void setUpInterrupts() {
   // LinkConnection
   irq_add(II_SERIAL, LINK_ISR_SERIAL);
   irq_add(II_TIMER3, LINK_ISR_TIMER);
-  irq_add(II_TIMER2, NULL);
 
   // A+B+START+SELECT
   REG_KEYCNT = 0b1100000000001111;
