4a54dbd93f0f1e38aacad4e5d9c6e436d66af9db
Rodrigo Alfonso <rodri042@gmail.com>
2023-11-21 09:10:44 -0300

    Reverting full reset to check out crash

--- src/utils/SceneUtils.h
+++ src/utils/SceneUtils.h
@@ -73,8 +73,7 @@ inline void SCENE_wait(u32 verticalLines) {
 inline void SCENE_softReset() {
   RUMBLE_stop();
   IOPORT_sdLow();
-  RegisterRamReset(RESET_VRAM | RESET_PALETTE | RESET_OAM | RESET_REG_SIO |
-                   RESET_REG_SOUND | RESET_REG);
+  RegisterRamReset(RESET_REG | RESET_VRAM);
   SoftReset();
 }
 
