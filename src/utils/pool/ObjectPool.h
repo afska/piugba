b8e21db7f1d192c86ee0111373aeec23703a0c4f
Rodrigo Alfonso <rodri042@gmail.com>
2023-12-04 06:07:33 -0300

    Debugging crash: Restoring old ObjectPool implementation

--- src/utils/pool/ObjectPool.h
+++ src/utils/pool/ObjectPool.h
@@ -4,9 +4,9 @@
 #include <libgba-sprite-engine/gba/tonc_bios.h>
 #include <libgba-sprite-engine/gba_engine.h>
 
-#include "IPoolable.h"
+#include <vector>
 
-const u32 MAX_OBJECT_POOL_SIZE = 50;
+#include "IPoolable.h"
 
 static bool isWarningVisible = false;
 inline void LOG_WARNING() {
@@ -28,23 +28,21 @@ class ObjectPool {
  public:
   template <typename F>
   ObjectPool(u32 size, F create) {
-    this->size = size;
     for (u32 i = 0; i < size; i++) {
-      objects[i].object = create(i);
-      activeIndices[i] = -1;
+      PooledObject<T>* pooledObject = new PooledObject<T>;
+      pooledObject->object = create(i);
+      objects.push_back(pooledObject);
     }
   }
 
   template <typename F>
   inline T* create(F initialize) {
-    for (u32 i = 0; i < size; i++) {
-      auto& it = objects[i];
-      if (!it.isActive) {
-        it.isActive = true;
-        activeIndices[activeObjects] = i;
+    for (auto& it : objects) {
+      if (!it->isActive) {
+        it->isActive = true;
         activeObjects++;
-        initialize(it.object);
-        return it.object;
+        initialize(it->object);
+        return it->object;
       }
     }
 
@@ -55,58 +53,48 @@ class ObjectPool {
     return NULL;
   }
 
-  inline T* getByIndex(u32 index) {
+  T* getByIndex(u32 index) {
     auto element = objects[index];
-    return element.isActive ? objects[index].object : NULL;
+    return element->isActive ? objects[index]->object : NULL;
   }
 
-  inline bool isFull() { return activeObjects == size; }
+  bool isFull() { return activeObjects == objects.size(); }
   u32 getActiveObjects() { return activeObjects; }
 
   void discard(u32 index) {
-    for (u32 i = 0; i < activeObjects; i++) {
-      if (activeIndices[i] == index) {
-        activeIndices[i] = activeIndices[activeObjects - 1];
-        activeIndices[activeObjects - 1] = -1;
-        activeObjects--;
-
-        ((IPoolable*)objects[index].object)->discard();
-        objects[index].isActive = false;
-        return;
-      }
-    }
+    ((IPoolable*)objects[index]->object)->discard();
+    objects[index]->isActive = false;
+    activeObjects--;
   }
 
   void clear() {
-    for (u32 i = 0; i < size; i++)
-      if (objects[i].isActive)
+    for (u32 i = 0; i < objects.size(); i++)
+      if (objects[i]->isActive)
         discard(i);
   }
 
   template <typename F>
   inline void forEach(F action) {
-    for (u32 i = 0; i < size; i++)
-      action(objects[i].object);
+    for (auto& it : objects)
+      action(it->object);
   }
 
   template <typename F>
   inline void forEachActive(F action) {
-    for (int i = activeObjects - 1; i >= 0; i--) {
-      u32 currentIndex = activeIndices[i];
-      if (objects[currentIndex].isActive)
-        action(objects[currentIndex].object);
-    }
+    for (auto& it : objects)
+      if (it->isActive)
+        action(it->object);
   }
 
   ~ObjectPool() {
-    for (u32 i = 0; i < size; i++)
-      delete objects[i].object;
+    for (auto& it : objects) {
+      delete it->object;
+      delete it;
+    }
   }
 
  private:
-  u32 size;
-  std::array<PooledObject<T>, MAX_OBJECT_POOL_SIZE> objects;
-  std::array<u32, MAX_OBJECT_POOL_SIZE> activeIndices;
+  std::vector<PooledObject<T>*> objects;
   u32 activeObjects = 0;
 };
 
