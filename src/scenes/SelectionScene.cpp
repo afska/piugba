5da76240b3c8234e4df497db32d20cdfa573fad8
Rodrigo Alfonso <rodri042@gmail.com>
2023-12-13 05:08:00 -0300

    FIX: Still showing variant when offset editing is enabled

--- src/scenes/SelectionScene.cpp
+++ src/scenes/SelectionScene.cpp
@@ -784,9 +784,10 @@ void SelectionScene::printNumericLevel(Chart* chart, s8 offsetY) {
       TextStream::instance().setText(
           (customOffset >= 0 ? "[+" : "[") + std::to_string(customOffset) + "]",
           TEXT_ROW - 1, -3);
-    } else if (chart->variant != '\0') {
-      SCENE_write(std::string("  ") + chart->variant, NUMERIC_LEVEL_ROW + 2);
     }
+
+    if (chart->variant != '\0')
+      SCENE_write(std::string("  ") + chart->variant, NUMERIC_LEVEL_ROW + 2);
 
     if (chart->difficulty == DifficultyLevel::NORMAL)
       return SCENE_write("NM", NUMERIC_LEVEL_ROW + offsetY);
