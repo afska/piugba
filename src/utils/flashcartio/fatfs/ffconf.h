de907cfe1e372290e1427627842c0e0dd6b07e22
Rodrigo Alfonso <rodri042@gmail.com>
2024-02-21 00:45:02 -0300

    Freeing up BSS by reducing max path length to 100 characters

--- src/utils/flashcartio/fatfs/ffconf.h
+++ src/utils/flashcartio/fatfs/ffconf.h
@@ -105,7 +105,7 @@ functions.
 */
 
 #define FF_USE_LFN 1
-#define FF_MAX_LFN 255
+#define FF_MAX_LFN 100
 /* The FF_USE_LFN switches the support for LFN (long file name).
 /
 /   0: Disable LFN. FF_MAX_LFN has no effect.
