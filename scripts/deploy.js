dabc3665315b0a1cfa2faba92298ed7339edbcda
Rodrigo Alfonso <rodri042@gmail.com>
2024-06-15 05:04:05 -0300

    Automatic compiling IPS patches

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,8 @@ const CONTENT_DIR = $path.join(ROOT_DIR, "src/data/content");
 const SONG_PACKS_DIR = $path.join(CONTENT_DIR, "songs-pack");
 const ROM_PACKS_DIR = $path.join(CONTENT_DIR, "roms");
 const DEV_DIR = $path.join(CONTENT_DIR, "roms/#dev");
+const FLIPS_FROM_CONTENT_DIR =
+  "../../../scripts/toolchain/programs/flips/flips.exe";
 const ROMNAME = "romname.txt";
 const ARCADE_SIGNAL = "ARCADE";
 const VARIANTS = ["arcade", "full"];
@@ -24,11 +26,26 @@ const SEARCH = process.argv[2];
 
 const log = (text) => console.log(`${SEPARATOR}${text}${SEPARATOR}`);
 const run = (command, options) => {
-  childProcess.execSync(command, {
+  return childProcess.execSync(command, {
     stdio: ["pipe", process.stdout, process.stderr],
     shell: true,
     ...options,
   });
+};
+const getPatches = () => {
+  try {
+    const output = run("git --no-pager branch", {
+      cwd: ROOT_DIR,
+      stdio: null,
+    }).toString();
+    return output
+      .split("\n")
+      .map((it) => it.trim().replace(/^\* /, ""))
+      .filter((it) => it.startsWith("patch_"));
+  } catch (e) {
+    console.error("Failed to get patches", e);
+    process.exit(1);
+  }
 };
 
 // -----------
@@ -36,9 +53,12 @@ const run = (command, options) => {
 // -----------
 
 if (!SEARCH) {
+  const patches = getPatches();
+
   VARIANTS.forEach((variant) => {
     ENVIRONMENTS.forEach((environment) => {
       log(`⌚  COMPILING: VARIANT=${variant}, ENV=${environment}`);
+      run("git checkout master", { cwd: ROOT_DIR });
       run("make clean", { cwd: ROOT_DIR });
       run("make assets", { cwd: ROOT_DIR });
       run(`make build ENV="${environment}" ${ARCADE_FLAG(variant)}`, {
@@ -47,6 +67,51 @@ if (!SEARCH) {
       fs.copyFileSync(
         $path.join(ROOT_DIR, OUTPUT_EMPTY),
         $path.join(CONTENT_DIR, OUTPUT_BUILDS(variant)[environment])
+      );
+
+      if (environment === "production") {
+        patches.forEach((patch) => {
+          const outputName = OUTPUT_BUILDS(variant)[environment].replace(
+            "piugba",
+            patch
+          );
+
+          log(
+            `⌚  COMPILING: VARIANT=${variant}, ENV=${environment}, PATCH=${patch}`
+          );
+          run(`git checkout ${patch}`, { cwd: ROOT_DIR });
+          run("make clean", { cwd: ROOT_DIR });
+          run("make assets", { cwd: ROOT_DIR });
+          run(`make build ENV="${environment}" ${ARCADE_FLAG(variant)}`, {
+            cwd: ROOT_DIR,
+          });
+          fs.copyFileSync(
+            $path.join(ROOT_DIR, OUTPUT_EMPTY),
+            $path.join(CONTENT_DIR, outputName)
+          );
+          run("git checkout master", { cwd: ROOT_DIR });
+        });
+      }
+    });
+  });
+
+  patches.forEach((patch) => {
+    VARIANTS.forEach((variant) => {
+      const cleanFile = OUTPUT_BUILDS(variant).production;
+      const patchedFile = OUTPUT_BUILDS(variant).production.replace(
+        "piugba",
+        patch
+      );
+
+      log(
+        `⌚  GENERATING IPS PATCH: VARIANT=${variant}, ENV=production, PATCH=${patch}`
+      );
+
+      run(
+        `${FLIPS_FROM_CONTENT_DIR} --create --ips ${cleanFile} ${patchedFile} ${patch}.${variant}.prod.ips`,
+        {
+          cwd: CONTENT_DIR,
+        }
       );
     });
   });
