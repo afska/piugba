ce942047a7c3ea92242333776ca126072de9e395
Rodrigo Alfonso <rodri042@gmail.com>
2025-06-15 03:59:16 -0300

    Updating hint

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,9 @@ extern "C" {
 LINK_VERSION_TAG EVERDRIVE_HINT =
     "This is a patch for the EverDrive flash cart. This flashcart can run PS/2 "
     "input and HQ mode at the same time thanks to its faster microSD reading "
-    "speed.";
+    "speed. DMA is replaced by a regular copy to allow serial interrupt "
+    "handlers to be triggered on time. For extra speed, ROM waitstates are "
+    "changed to 2,1.";
 
 // Emulators and flashcarts use this string to autodetect the save type
 const char* SAVEFILE_TYPE_HINT = "SRAM_Vnnn\0\0";
